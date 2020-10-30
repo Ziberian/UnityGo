5ffe885eefd6f9de122c290aa498c1c2e9e6583c
Ziberian <emref99@hotmail.com>
2020-10-30 16:49:17 -0400

    The game itself

---
+++ UnityGo/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,250 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerActionControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerActionControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Land"",
+            ""id"": ""8923272c-7285-48bf-b1ec-0d2c8ef773f5"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""a161680a-b2b5-4fc9-b9bd-4a5afbc6b3cc"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""8b3bf489-e86d-4960-b1cd-66a95208196b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Sideways"",
+                    ""id"": ""b5a3c87a-60fb-46af-973a-8fcd8e42ab47"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""3bc68e2f-77b7-4171-aeaa-bc2ed06ac69a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""e47bf248-2445-4157-baac-646a6f9039b9"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""70b13981-9ee5-4dca-a084-553c3dfa89f9"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Water"",
+            ""id"": ""be165772-d74b-46d1-8b32-73759accccc2"",
+            ""actions"": [
+                {
+                    ""name"": ""New action"",
+                    ""type"": ""Button"",
+                    ""id"": ""021a2a88-ee5d-4002-99e8-3191c6dfdde2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""ea5058c9-9876-4aad-b16d-3011e0ad2802"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Land
+        m_Land = asset.FindActionMap("Land", throwIfNotFound: true);
+        m_Land_Move = m_Land.FindAction("Move", throwIfNotFound: true);
+        m_Land_Jump = m_Land.FindAction("Jump", throwIfNotFound: true);
+        // Water
+        m_Water = asset.FindActionMap("Water", throwIfNotFound: true);
+        m_Water_Newaction = m_Water.FindAction("New action", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Land
+    private readonly InputActionMap m_Land;
+    private ILandActions m_LandActionsCallbackInterface;
+    private readonly InputAction m_Land_Move;
+    private readonly InputAction m_Land_Jump;
+    public struct LandActions
+    {
+        private @PlayerActionControls m_Wrapper;
+        public LandActions(@PlayerActionControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Land_Move;
+        public InputAction @Jump => m_Wrapper.m_Land_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Land; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LandActions set) { return set.Get(); }
+        public void SetCallbacks(ILandActions instance)
+        {
+            if (m_Wrapper.m_LandActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_LandActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public LandActions @Land => new LandActions(this);
+
+    // Water
+    private readonly InputActionMap m_Water;
+    private IWaterActions m_WaterActionsCallbackInterface;
+    private readonly InputAction m_Water_Newaction;
+    public struct WaterActions
+    {
+        private @PlayerActionControls m_Wrapper;
+        public WaterActions(@PlayerActionControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Newaction => m_Wrapper.m_Water_Newaction;
+        public InputActionMap Get() { return m_Wrapper.m_Water; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(WaterActions set) { return set.Get(); }
+        public void SetCallbacks(IWaterActions instance)
+        {
+            if (m_Wrapper.m_WaterActionsCallbackInterface != null)
+            {
+                @Newaction.started -= m_Wrapper.m_WaterActionsCallbackInterface.OnNewaction;
+                @Newaction.performed -= m_Wrapper.m_WaterActionsCallbackInterface.OnNewaction;
+                @Newaction.canceled -= m_Wrapper.m_WaterActionsCallbackInterface.OnNewaction;
+            }
+            m_Wrapper.m_WaterActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Newaction.started += instance.OnNewaction;
+                @Newaction.performed += instance.OnNewaction;
+                @Newaction.canceled += instance.OnNewaction;
+            }
+        }
+    }
+    public WaterActions @Water => new WaterActions(this);
+    public interface ILandActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+    public interface IWaterActions
+    {
+        void OnNewaction(InputAction.CallbackContext context);
+    }
+}
---
+++ UnityGo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerActionControls playerActionControls;
+
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private float jumpSpeed = 5.0f;
+    [SerializeField] private LayerMask ground;
+
+    private Rigidbody2D rb;
+
+    private Collider2D col;
+
+    private void Awake()
+    {
+        playerActionControls = new PlayerActionControls();
+    }
+
+    private void OnEnable()
+    {
+        playerActionControls.Enable();
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void OnDisable()
+    {
+        playerActionControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerActionControls.Land.Jump.performed += _ => Jump();
+    }
+
+    private void Jump()
+    {
+        if(IsGrounded())
+        {
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector2 feetPos = transform.position;
+        feetPos.y -= col.bounds.extents.y;
+        return Physics2D.OverlapCircle(feetPos, .1f, ground);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //read the movement value
+        float movementInput = playerActionControls.Land.Move.ReadValue<float>();
+
+        //move player
+        Vector3 currentPosition = transform.position;
+        currentPosition.x += movementInput * speed * Time.deltaTime;
+        transform.position = currentPosition;
+
+    }
+}
